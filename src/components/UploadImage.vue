28dcd07cd0eb372079f58c33821d3048bf18218d
IanWei <ikaeuu@gmail.com>
2019-02-11 10:44:43 +1100

    bugs fix

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -52,7 +52,7 @@
             this.hasImage = true;
             name = file.name;
             if (name.length > 14) {
-              this.filename = name.slice(0, 18).concat(' …');
+              this.filename = name.slice(0, 16).concat(' …');
             } else {
               this.filename = name;
             }
